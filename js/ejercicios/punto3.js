fca954ce2184d917ca38a0a1e2ab322cda0f2c89
Cami <camilafuentesmaldonado@gmail.com>
2022-08-08 17:53:34 -0300

    punto 3 realizado

---
+++ js/ejercicios/punto3.js
@@ -0,0 +1,27 @@
+// 3-Escribe una clase que permita crear distintos objetos “rectángulos”, con las propiedades de alto y ancho, mas los métodos necesarios para modificar y mostrar sus propiedades, calcular el perímetro y el área
+
+class Rectangulo {
+    constructor(alto,ancho){
+        this.alto=alto;
+        this.ancho=ancho;
+    }
+    //metodos
+    set cambiarAlto( alto=0 ) {
+        this.alto = alto;
+      }
+    
+      set cambiarAncho ( ancho = 0 ) {
+        this.ancho = ancho;
+      }
+    
+      get area() {
+        return this.alto * this.ancho;
+      }
+}
+    
+    let obj = new Rectangulo();
+
+    obj.cambiarAlto = 10;
+    obj.cambiarAncho = 20;
+    obj.area;
+    
